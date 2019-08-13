ab4a832648bc22e38451869414d80b3cc517ad10
Vytas88 <vytas.tenys@gmail.com>
2019-08-13 19:40:19 +0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: {
+    app: "./src/index.js"
+  },
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "[name].[hash].js"
+  },
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.(s*)css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: { hmr: process.env.NODE_ENV === "development" }
+          },
+          "css-loader",
+          "sass-loader"
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./static/index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].[hash].css",
+      chunkFilename: "[id].css",
+      ignoreOrder: false
+    })
+  ]
+};
